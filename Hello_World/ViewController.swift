f78f671ac40d5298426fe4b5b982405e4c2758e1
Gleb Trubanov <crew.gleb@gmail.com>
2021-12-25 23:28:49 +0300

    Initial Commit

---
+++ Hello_World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello_World
+//
+//  Created by Надежда Говор on 25.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
