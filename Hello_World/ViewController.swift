2857e6c0a9abb61780ede046b9b7e954338e0ee8
Gleb Trubanov <crew.gleb@gmail.com>
2021-12-26 19:44:21 +0300

    Add logic in button

--- Hello_World/ViewController.swift
+++ Hello_World/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButton: UIButton!
+    
+    @IBOutlet var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 27
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if helloWorldLabel.isHidden == true {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
 }
+
 
